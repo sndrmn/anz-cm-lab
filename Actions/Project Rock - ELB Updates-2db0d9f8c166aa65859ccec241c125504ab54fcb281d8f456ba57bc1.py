9f7ff0e34468ffb5a6a61d8a762446a74d9d99cf
Sander <35436862+sndrmn@users.noreply.github.com>
2020-04-16 22:06:46 +1000

    Add files via upload

--- Actions/Project Rock - ELB Updates-2db0d9f8c166aa65859ccec241c125504ab54fcb281d8f456ba57bc1.py
+++ Actions/Project Rock - ELB Updates-2db0d9f8c166aa65859ccec241c125504ab54fcb281d8f456ba57bc1.py
@@ -2,7 +2,6 @@ import boto3
 def handler(context, inputs):
     SSLSingapore = "arn:aws:acm:ap-southeast-1:994172548385:certificate/3bae16ae-ac3f-4c42-bc79-5c114777e751"
     SSLSydney = "arn:aws:acm:ap-southeast-2:994172548385:certificate/c7f0a436-f494-4525-a566-fa1da31fc175"
-    SendSMS = "No"
         
     Region = str(inputs['tags']['Region'])
     Platform = str(inputs['tags']['Platform'])
@@ -32,29 +31,6 @@ def handler(context, inputs):
                  },
              ]
              )
-             result = client.describe_load_balancers(LoadBalancerNames=[x])
-             sg = [sg['SecurityGroups'] for sg in result['LoadBalancerDescriptions']]
-             sg = str(sg)
-             sg = sg.replace("[['", '')
-             sg = (sg.replace("']]", ''))
-
-             ec2 = boto3.client('ec2', region_name=region_code)
-             result2 = ec2.authorize_security_group_ingress( 
-             GroupId=sg, 
-             IpPermissions=[
-              {'IpProtocol': 'TCP', 
-               'IpRanges': [
-                   {
-                       'CidrIp': '0.0.0.0/0'
-                   },
-                 ],
-               'FromPort': 443, 
-               'ToPort': 443,
-               'UserIdGroupPairs': [{'GroupId' : sg}]
-              }
-             ],
-             )
     outputs = {
-      "SendSMS": SendSMS 
     }
     return outputs
