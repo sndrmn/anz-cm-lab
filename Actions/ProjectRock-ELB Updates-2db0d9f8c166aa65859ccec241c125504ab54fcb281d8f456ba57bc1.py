48ee1222e47740ea551b5a69104c17bc678e3fc3
Sander <35436862+sndrmn@users.noreply.github.com>
2020-05-11 12:07:58 +1000

    Add files via upload

---
+++ Actions/ProjectRock-ELB Updates-2db0d9f8c166aa65859ccec241c125504ab54fcb281d8f456ba57bc1.py
@@ -0,0 +1,39 @@
+import boto3
+def handler(context, inputs):
+    #Originally had Demo working in AWS SIN & SYD Regions have changed demo to only work in SYD.  
+    #Code kept same incase I change my mind in future
+    
+    SSLSingapore = "arn:aws:acm:ap-southeast-1:994172548385:certificate/3bae16ae-ac3f-4c42-bc79-5c114777e751"
+    SSLSydney = "arn:aws:acm:ap-southeast-2:994172548385:certificate/c7f0a436-f494-4525-a566-fa1da31fc175"
+        
+    Region = str(inputs['tags']['Region'])
+    Platform = str(inputs['tags']['Platform'])
+    if Platform == "platform:aws":
+      if Region == "region:sydney":
+          SSL = SSLSydney
+          region_code = "ap-southeast-2"
+      else:
+          SSL = SSLSingapore
+          region_code = "ap-southeast-1"
+      print(region_code)
+      client = boto3.client('elb', region_name=region_code)
+      response = client.describe_load_balancers(LoadBalancerNames=[])
+      elb = [elb['LoadBalancerName'] for elb in response['LoadBalancerDescriptions']]
+      for x in elb:
+         if "projectrock" in x:
+             print(x)
+             response = client.create_load_balancer_listeners(
+             LoadBalancerName = x,
+             Listeners=[
+                 {
+                     'Protocol': 'https',
+                     'LoadBalancerPort': 443,
+                     'InstanceProtocol': 'http',
+                     'InstancePort': 80,
+                     'SSLCertificateId': SSL
+                 },
+             ]
+             )
+    outputs = {
+    }
+    return outputs
